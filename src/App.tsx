0e5087f60135a23725a51b971867a84f1ce72e5f
Marian Kmotorka <Marian.Kmotorka@quest.com>
2021-09-29 23:04:20 +0200

    react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,42 @@
-import { useState } from 'react'
-import './App.css'
 import UseStateWithHistoryExample from './examples/UseStateWithHistoryExample'
+import { BrowserRouter as Router, Switch, Route, NavLink, Redirect } from 'react-router-dom'
+import './App.css'
 
 function App() {
-  const [activeHook, setActiveHook] = useState(0)
-
   return (
-    <div className='container'>
-      <div className='menu'>
-        <h1>my hooks</h1>
+    <Router>
+      <div className='container'>
+        <div className='menu'>
+          <h1>my hooks</h1>
 
-        <div className='item active'>useStateWithHistory</div>
-        <div className='item'>useToggle</div>
-        <div className='item'>useTimeout</div>
+          <NavLink className='item' to='/useStateWithHistory'>
+            useStateWithHistory
+          </NavLink>
+
+          <NavLink className='item' to='/useToggle'>
+            useToggle
+          </NavLink>
+
+          <NavLink className='item' to='/useTimeout'>
+            useTimeout
+          </NavLink>
+
+          <NavLink className='item' to='/useArray'>
+            useArray
+          </NavLink>
+        </div>
+
+        <div className='content'>
+          <Switch>
+            <Route path='/useStateWithHistory' component={() => <UseStateWithHistoryExample />} />
+            <Route path='/useToggle' component={() => <p>Work in progress...</p>} />
+            <Route path='/useTimeout' component={() => <p>Work in progress...</p>} />
+            <Route path='/useArray' component={() => <p>Work in progress...</p>} />
+            <Redirect to='/useStateWithHistory' />
+          </Switch>
+        </div>
       </div>
-      <div className='content'>{activeHook === 0 && <UseStateWithHistoryExample />}</div>
-    </div>
+    </Router>
   )
 }
 
--- src/examples/UseStateWithHistoryExample.tsx
+++ src/examples/UseStateWithHistoryExample.tsx
@@ -1,4 +1,6 @@
+import SyntaxHighlighter from 'react-syntax-highlighter'
 import useStateWithHistory from '../hooks/useStateWithHistory'
+import { github } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 
 const UseStateWithHistoryExample = () => {
   const [value, setValue, { history, cursor, goBack, goForward }] = useStateWithHistory(1)
@@ -16,6 +18,55 @@ const UseStateWithHistoryExample = () => {
       <button onClick={() => setValue(x => 2 * x)}>DOUBLE</button>
       <button onClick={goBack}>GO BACK</button>
       <button onClick={goForward}>GO FORWARD</button>
+
+      <SyntaxHighlighter language='typescript' style={github}>
+        {`import { useCallback, useRef, useState } from 'react'
+
+type DefaultValue<T> = T | (() => T)
+type SetParameter<T> = T | ((prev: T) => T)
+
+const useStateWithHistory = <T>(defaultValue: DefaultValue<T>, capacity = 10) => {
+  const [value, setValue] = useState<T>(defaultValue)
+  const history = useRef<T[]>([value])
+  const cursor = useRef<number>(0)
+
+  const set = (parameter: SetParameter<T>) => {
+    const resolvedValue: T =
+      typeof parameter === 'function' ? (parameter as Function)(value) : parameter
+
+    if (history.current[cursor.current] === resolvedValue) return
+
+    if (cursor.current < history.current.length - 1)
+      history.current = history.current.splice(0, cursor.current + 1)
+
+    history.current.push(resolvedValue)
+
+    if (history.current.length > capacity) history.current.shift()
+
+    cursor.current = history.current.length - 1
+    setValue(resolvedValue)
+  }
+
+  const goBack = useCallback(() => {
+    if (cursor.current === 0) return
+    setValue(history.current[--cursor.current])
+  }, [])
+
+  const goForward = useCallback(() => {
+    if (cursor.current === history.current.length - 1) return
+    setValue(history.current[++cursor.current])
+  }, [])
+
+  return [
+    value,
+    set,
+    { history: history.current, cursor: cursor.current, goBack, goForward }
+  ] as const
+}
+
+export default useStateWithHistory
+`}
+      </SyntaxHighlighter>
     </div>
   )
 }
