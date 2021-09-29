951a0220250e68a91c5983092ad7d109dd45a86d
Marian Kmotorka <Marian.Kmotorka@quest.com>
2021-09-29 20:49:03 +0200

    stupid styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import { useState } from 'react'
 import './App.css'
 import UseStateWithHistoryExample from './examples/UseStateWithHistoryExample'
 
 function App() {
-  return <UseStateWithHistoryExample />
+  const [activeHook, setActiveHook] = useState(0)
+
+  return (
+    <div className='container'>
+      <div className='menu'>
+        <h1>my hooks</h1>
+
+        <div className='item active'>useStateWithHistory</div>
+        <div className='item'>useToggle</div>
+        <div className='item'>useTimeout</div>
+      </div>
+      <div className='content'>{activeHook === 0 && <UseStateWithHistoryExample />}</div>
+    </div>
+  )
 }
 
 export default App
---
+++ src/hooks/useTimeout.ts
@@ -0,0 +1,3 @@
+const useTimeout = () => {}
+
+export default useTimeout
