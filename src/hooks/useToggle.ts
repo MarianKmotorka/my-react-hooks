1c6400c9980314ce245ebbcb84fa8d4dce1cecf2
Marian Kmotorka <Marian.Kmotorka@quest.com>
2021-10-07 16:26:07 +0200

    useTimeout

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -1,3 +1,7 @@
-const useToggle = () => {}
+import { useState } from 'react'
+
+const useToggle = (values: any[]) => {
+  const [value, setValue] = useState(values[0])
+}
 
 export default useToggle
