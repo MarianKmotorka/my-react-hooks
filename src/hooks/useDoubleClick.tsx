aff3be4066b669ae1b78094e4e5ffef9153ec239
Marian Kmotorka <Marian.Kmotorka@quest.com>
2021-10-29 13:01:50 +0200

    useDoubleClick template

---
+++ src/hooks/useDoubleClick.tsx
@@ -0,0 +1,9 @@
+import { useRef } from 'react'
+
+//TODO: Implement
+const useDoubleClick = (callback: () => void, maxTimeBetweenClicks = 300) => {
+  const element = useRef<HTMLElement>()
+  return element
+}
+
+export default useDoubleClick
